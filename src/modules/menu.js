b7bc946718b4c74b07066f95cf52c55643c25c13
Kat Tyushnyakova <avchimchik@gmail.com>
2024-10-09 00:52:02 +0500

    И пока все

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,17 @@
-const two = () => {
-    console.log(`Я модуль номер 2!`)
+const menu = () => {
+    const menuBtn = document.querySelector('.menu');
+    const menu = document.querySelector('menu');
+    const closeBtn = menu.querySelector('.close-btn');
+    const menuItems = menu.querySelectorAll('ul>li>a');
+
+    const handleMenu = () => {
+        menu.classList.toggle('active-menu')
+    }
+
+    menuBtn.addEventListener('click', handleMenu);
+    closeBtn.addEventListener('click', handleMenu);
+
+    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu));
 }
 
-export default two
+export default menu
