3e714b314ab033740a931a39bf5219cbe0906ba2
Kat Tyushnyakova <avchimchik@gmail.com>
2024-10-09 19:31:09 +0500

    Код по видео работает

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,5 +1,17 @@
 const modal = () => {
+    const modalWindow = document.querySelector('.popup');
+    const buttons = document.querySelectorAll('.popup-btn');
+    const closeBtn = modalWindow.querySelector('.popup-close');
 
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modalWindow.style.display = 'block';
+        })
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modalWindow.style.display = 'none';
+    })
 }
 
 export default modal
