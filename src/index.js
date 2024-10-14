744a016d09725ef631dbaaa455051c7aaa79671a
Kat Tyushnyakova <avchimchik@gmail.com>
2024-10-14 21:13:39 +0500

    Работает валидация ввода

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import click from "./modules/click";
 import calc from "./modules/calc";
-import footerForm from "./modules/footerForm";
+import inputBan from "./modules/inputBan";
 
 timer('16 october 2024');
 menu();
 modal();
 click();
 calc();
-footerForm();
+inputBan();
