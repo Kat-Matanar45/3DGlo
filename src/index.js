dd2c01aaa65693f5485574e82dcc4d76cd8c9e14
Kat Tyushnyakova <avchimchik@gmail.com>
2024-10-14 17:35:59 +0500

    Создан файл для блока калькулятора

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import click from "./modules/click";
+import calc from "./modules/calc";
 
 timer('16 october 2024');
 menu();
 modal();
 click();
+calc ();
