ceed815f13e4fc3c6e17b7502c96c5fa1d28e096
Kat Tyushnyakova <avchimchik@gmail.com>
2024-10-21 15:50:23 +0500

    Починила код

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -13,17 +13,20 @@ document.body.addEventListener('click', (e) => {
 
     if (e.target.classList.contains('close-btn') || (!e.target.closest('menu') && !e.target.closest('.menu'))) {
         menu.classList.remove('active-menu');
-    }
+    };
 
-    if (e.target.tagName === 'A' && e.target.closest('menu')) {
+    if ((e.target.tagName === 'A') && (e.target.closest('menu'))) {
         e.preventDefault(); 
         id = e.target.getAttribute('href'); 
         menu.classList.remove('active-menu'); 
+
+        if (id !== '#close') {
         document.querySelector(id).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
-        });
-    }
+        }); 
+    };
+    };
 });
 }
 
